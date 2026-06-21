6ef4cd4f813b33f34650add3eefd29991e5d6d3f
Steven Keith Milne <st10493409@vcconnect.edu.za>
2026-06-21 23:16:34 +0200

    prog
    prog

--- CyberSecurityChatbot/obj/Debug/net10.0-windows/App.g.i.cs
+++ CyberSecurityChatbot/obj/Debug/net10.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace CyberSecurityChatbot {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.9.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace CyberSecurityChatbot {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.9.0")]
         public static void Main() {
             CyberSecurityChatbot.App app = new CyberSecurityChatbot.App();
             app.InitializeComponent();
--- CyberSecurityChatbot/obj/Debug/net10.0-windows/CyberSecurityChatbot.AssemblyInfo.cs
+++ CyberSecurityChatbot/obj/Debug/net10.0-windows/CyberSecurityChatbot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CyberSecurityChatbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3979eecb43d74716c191b43e5f47a123f88cafc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5db77d5ebdcd33a74e5c08403074d6bbe08bde56")]
 [assembly: System.Reflection.AssemblyProductAttribute("CyberSecurityChatbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CyberSecurityChatbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
